6967036e1ae077f738138623506bf547649b09bc
chantzish <chantzish@gmail.com>
2020-06-13 22:52:51 +0300

    draft 17

--- launch.sh
+++ launch.sh
@@ -13,5 +13,6 @@ cd /opt/noVNC && ./utils/launch.sh --listen 6080 --vnc localhost:5901 &
 /opt/noVNC/utils/websockify/run 6090 localhost:2200 --heartbeat=45 &
 printf "%s" "$AUTH_KEYS" > .ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+ssh-keygen -A
 /usr/sbin/sshd -p 2200 -o AuthorizedKeysFile=/home/user/.ssh/authorized_keys &
 nginx -g 'daemon off;'
